a9f9f1e1aa388e6786eca85014977e7ba8249646
NeleWandel <109591602+NeleWandel@users.noreply.github.com>
2022-07-19 13:35:09 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
-
-release = '0.1'
-version = '0.1.0'
+project = 'TDS'
+copyright = '2022, Nele Wandel'
+author = 'Nele Wandel'
 
 # -- General configuration
 
