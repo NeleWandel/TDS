7abbfd3ae4bc03c431addd2723a2ddc110e45aaf
NeleWandel <109591602+NeleWandel@users.noreply.github.com>
2022-08-12 19:32:27 +0200

    Update Spectra.py

--- Spectra.py
+++ Spectra.py
@@ -13,20 +13,21 @@ sp = rm.open_resource('ASRL3::INSTR', read_termination = '\r',
 def Run(wavelength):
     sp.write(str(wavelength) + ' GOTO')
 
-def Scan(scanrate, resolution=None):
+#Go to a designated wavelength at a speed set by scanrate in nm per minute
+def Scan(wavelength, scanrate=None):
     if scanrate:
         sp.write(str(scanrate) + ' NM/MIN')
         time.sleep(0.1)
     sp.write(str(wavelength) + ' NM')
 
-
 #initialises the monochromator to default values
-def Reset():
+def Restart():
     sp.write('HELLO')
 
-#sets initialisation wavelength 
+#sets default wavelength in nm, does not change the current wavelength as it only applies after initialising the instrument
 def InitialWavelength(wavelength):
     sp.write(str(wavelength) + ' INIT-WAVELENGTH')
-
+    
+#sets default scanrate in nm/min, does not change the current scanrate as it only applies after initialising the instrument
 def InitialScanrate(scanrate):
     sp.write(str(scanrate) + ' INIT-SRATE')
