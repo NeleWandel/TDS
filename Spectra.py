58db76344e38754bc0e3b82ae6823edd78120268
NeleWandel <109591602+NeleWandel@users.noreply.github.com>
2022-08-12 16:30:09 +0200

    Create Spectra.py

---
+++ Spectra.py
@@ -0,0 +1,32 @@
+import pyvisa
+from pyvisa.constants import StopBits, Parity
+import time
+rm = pyvisa.ResourceManager()
+print(rm.list_resources())
+
+sp = rm.open_resource('ASRL3::INSTR', read_termination = '\r', 
+                      write_termination = '\r', baud_rate = 9600, 
+                      data_bits = 8, flow_control = 2, 
+                      parity = Parity.none, stop_bits = StopBits.one)
+
+#Go to a designated wavelength at max speed
+def Run(wavelength):
+    sp.write(str(wavelength) + ' GOTO')
+
+def Scan(wavelength):
+    sp.write(str(wavelength) + ' NM')
+    
+#Set Resolution in nanometer per minute
+def SetResolution(perminute):
+    sp.write(str(perminute) + ' NM/MIN')
+
+#initialises the monochromator to default values
+def Reset():
+    sp.write('HELLO')
+
+#sets initialisation wavelength 
+def InitialWavelength(wavelength):
+    sp.write(str(wavelength) + ' INIT-WAVELENGTH')
+
+def InitialScanrate(scanrate):
+    sp.write(str(scanrate) + ' INIT-SRATE')
